2685676b85a1c164311545661f90a1636051ed31
Xapacheco <112705657+Xapacheco@users.noreply.github.com>
2026-08-15 09:06:17 +0200

    Añadir sitemap y configuracion SEO basica

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,15 @@
+﻿export const prerender = true;
+
+export function GET() {
+  const body = `User-agent: *
+Allow: /
+
+Sitemap: https://orixe-galicia.github.io/sitemap.xml
+`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+    },
+  });
+}
---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,26 @@
+﻿import { getAllMissions } from "../content";
+
+export const prerender = true;
+
+export async function GET() {
+  const site = "https://orixe-galicia.github.io";
+  const missions = await getAllMissions();
+
+  const urls = [
+    `${site}/`,
+    `${site}/misiones/`,
+    `${site}/mapa/`,
+    ...missions.map((mission) => `${site}/misiones/${mission.data.slug}/`),
+  ];
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map((url) => `  <url><loc>${url}</loc></url>`).join("\n")}
+</urlset>`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
+}
